c7b46015b04ed3652da57cdcbd1f6d8cdd1daf0d
playerxq <1062842783@qq.com>
2016-11-22 16:19:19 +0800

    test
    test

---
+++ test/test/test.cpp
@@ -0,0 +1,21 @@
+// test.cpp : 定义控制台应用程序的入口点。
+//
+#include "stdafx.h"
+#include <cstdio>
+#include <iostream>
+#include <string.h>
+#include <math.h>
+#include <vector>
+#include <queue>
+#include<iomanip>
+#include <ctype.h>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+	printf("hhh");
+}
+
